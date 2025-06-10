d7f29cd0effabb8ca87f7ad837dc930b31a38164
Julio Cesar Martin <135804676+juliocesar-tkww@users.noreply.github.com>
2025-06-10 12:25:02 +0200

    refactor: type

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'react-s3-uploader' {
-  import { Component } from 'react';
+  import { Component, RefObject } from 'react';
 
   export interface S3Response {
     signedUrl: string;
@@ -29,7 +29,7 @@ declare module 'react-s3-uploader' {
     uploadRequestHeaders?: object;
     contentDisposition?: string;
     server?: string;
-    inputRef?: (ref: HTMLInputElement) => any;
+    inputRef?: RefObject<HTMLInputElement | null>;
     autoUpload?: boolean;
     scrubFilename?: (filename: string) => string;
     [key: string]: any;
